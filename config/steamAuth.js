27c05751d18445b8d9411bd9329d571aa5fb131a
Jamil Sinno <jamil.sinno@gmail.com>
2022-09-02 19:29:58 -0300

    added .env and changed steamAuth to dynamic apiKey

--- config/steamAuth.js
+++ config/steamAuth.js
@@ -1,9 +1,10 @@
 const SteamAuth = require("node-steam-openid");
+const apiKey = process.env.STEAM_API_KEY
 
 const steam = new SteamAuth({
     realm: `https://localhost:${process.env.PORT}`, // Site name displayed to users on logon
   returnUrl: `https://localhost:${process.env.PORT}/steam/updateUser`, // Your return route
-  apiKey: "9B4E063468097CEECB414E4AA3BA5F6E" // Steam API key
+  apiKey: apiKey // Steam API key
 });
 
 module.exports = steam
