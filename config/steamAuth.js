fcf4de410480b05550ed5164f4a769d05e7a74d8
Gabriel Rodriguez <garodri97@gmail.com>
2022-09-02 15:28:12 -0700

    changed steam api key to process env

--- config/steamAuth.js
+++ config/steamAuth.js
@@ -3,7 +3,7 @@ const SteamAuth = require("node-steam-openid");
 const steam = new SteamAuth({
     realm: `https://localhost:${process.env.PORT}`, // Site name displayed to users on logon
   returnUrl: `https://localhost:${process.env.PORT}/steam/updateUser`, // Your return route
-  apiKey: "9B4E063468097CEECB414E4AA3BA5F6E" // Steam API key
+  apiKey: process.env.STEAM_API_KEY // Steam API key
 });
 
 module.exports = steam
