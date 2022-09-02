eb0dba81a3927a85a207baba172b6f5423fe7ddb
Gabriel Rodriguez <garodri97@gmail.com>
2022-09-02 15:46:50 -0700

    add dotenv to steamAuth config

--- config/steamAuth.js
+++ config/steamAuth.js
@@ -1,4 +1,5 @@
 const SteamAuth = require("node-steam-openid");
+require('dotenv').config({path: './config/.env'})
 
 const steam = new SteamAuth({
     realm: `https://localhost:${process.env.PORT}`, // Site name displayed to users on logon
