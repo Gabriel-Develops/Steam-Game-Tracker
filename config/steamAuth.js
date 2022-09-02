5afc956b7be79bdaf43e10f5f92abe8efc7cab33
Jamil Sinno <jamil.sinno@gmail.com>
2022-09-02 19:39:33 -0300

    merged from main and fixed error

--- config/steamAuth.js
+++ config/steamAuth.js
@@ -1,14 +1,9 @@
 const SteamAuth = require("node-steam-openid");
-const apiKey = process.env.STEAM_API_KEY
 
 const steam = new SteamAuth({
-    realm: `https://localhost:${process.env.PORT}`, // Site name displayed to users on logon
+  realm: `https://localhost:${process.env.PORT}`, // Site name displayed to users on logon
   returnUrl: `https://localhost:${process.env.PORT}/steam/updateUser`, // Your return route
-<<<<<<< HEAD
-  apiKey: apiKey // Steam API key
-=======
   apiKey: process.env.STEAM_API_KEY // Steam API key
->>>>>>> main
 });
 
 module.exports = steam
