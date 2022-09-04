55faa0244e2e7ca68ec682c9bfcf04892a8e98db
Dana Arroway <dana.arroway@gmail.com>
2022-09-03 21:11:15 -0500

    Put Alex's changes to server js back on. Sorry Alex, I'll be more careful in the future

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
-const steamRoutes = require('./routes/steam')
-const fetch = require('node-fetch')
+
 
 // Setup config
 require('dotenv').config({path: './config/.env'})
@@ -33,14 +31,57 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
 // Sessions
-app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+
+
+
+const todoRoutes = require('./routes/todos')
+const steamRoutes = require('./routes/steam')
+const fetch = require('node-fetch')
+
+// Setup config
+require('dotenv').config({path: './config/.env'})
+require('./config/passport')(passport)
+require('./config/steamAuth')
+
+
+function setupMiddleware() {
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(logger('dev'))
+    // Sessions
+    app.use(
+        session({
+            secret: 'keyboard cat',
+            resave: false,
+            saveUninitialized: false,
+            store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        })
+    )
+    // Passport middleware
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    app.use(flash())
+}
+
+function setupRoutes() {
+    app.use('/', mainRoutes)
+    app.use('/todos', todoRoutes)
+    app.use('/steam', steamRoutes)
+}
+
+async function startServer() {
+    // Wait for db connection before starting server, to avoid bugs where the server starts before the db is ready.
+    // (i.e, when nodemon automatically refreshes after changes are made to the server and immediately tries to query the db.)
+    await connectDB()
+    setupMiddleware()
+    setupRoutes()
+    app.listen(process.env.PORT, ()=>{
+        console.log('Server is running, you better catch it!')
     })
-)
+}
 
 
 // ----------------------------------------
