203f371ef816af4b0c1b98d499f1077ad8dbb437
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-03 19:59:52 -0500

    Ensure database connection resolves before server makes requests (...) - Refactor server setup process into functions for organization and ensuring order of operations. - Wait for db connection before starting server, to avoid bugs where the server starts before the db is ready. i.e., when nodemon automatically refreshes after changes are made to the server and the server immediately tries to query the db without waiting for connection.

--- server.js
+++ server.js
@@ -12,40 +12,52 @@ const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 const steamRoutes = require('./routes/steam')
 const fetch = require('node-fetch')
-require('dotenv').config({path: './config/.env'})
 
-// Passport config
+// Setup config
+require('dotenv').config({path: './config/.env'})
 require('./config/passport')(passport)
 require('./config/steamAuth')
 
-connectDB()
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
-// Sessions
-app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+function setupMiddleware() {
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(logger('dev'))
+    // Sessions
+    app.use(
+        session({
+            secret: 'keyboard cat',
+            resave: false,
+            saveUninitialized: false,
+            store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        })
+    )
+    // Passport middleware
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    app.use(flash())
+}
+
+function setupRoutes() {
+    app.use('/', mainRoutes)
+    app.use('/todos', todoRoutes)
+    app.use('/steam', steamRoutes)
+}
+
+async function startServer() {
+    // Wait for db connection before starting server, to avoid bugs where the server starts before the db is ready.
+    // (i.e, when nodemon automatically refreshes after changes are made to the server and immediately tries to query the db.)
+    await connectDB()
+    setupMiddleware()
+    setupRoutes()
+    app.listen(process.env.PORT, ()=>{
+        console.log('Server is running, you better catch it!')
     })
-  )
-  
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+}
 
-app.use(flash())
 
-// Main routes has all main routes
-app.use('/', mainRoutes)
-app.use('/steam', steamRoutes)
-app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+// ----------------------------------------
+startServer()
