f69189bdcbc27d515522002c51ad236a99f62bdb
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-03 21:29:18 -0500

    Create game data schema for use as embedded object within UserSchema.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,22 @@
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
+// Embedded document for game info retrieved from Steam API
+const GameSchema = new mongoose.Schema({
+    appid: Number,
+    name: String,
+    playtime_forever: Number,
+    img_icon_url: String,
+    has_community_visible_stats: Boolean,
+    playtime_windows_forever: Number,
+    playtime_mac_forever: Number,
+    playtime_linux_forever: Number,
+    rtime_last_played: Number
+})
+
+
 const UserSchema = new mongoose.Schema({
-    //Won't be using userName
+  //Won't be using userName
   //userName: { type: String, unique: true },
   email: { 
     type: String, 
@@ -10,9 +24,8 @@ const UserSchema = new mongoose.Schema({
   password: String,
   steamUserName: String,
   steamID: String,
-  // could createe a schema for games and insert into the array here as [gamesSchema]
-  // for now it will be an array of appids
-  gamesOwned: [String]
+  // Array of game objects containing info about each of the user's owned games
+  ownedGames: [GameSchema]
 })
 
 
