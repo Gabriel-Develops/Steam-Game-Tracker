fb74c0f735a8c46f38fb7196972468787c2abf30
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-03 23:45:38 -0500

    Add logic for handling missing steam api key environment variable gracefully (...) - Console log an error that makes sense, with instructions to fix. - Exit the process gracefully.

--- config/steamAuth.js
+++ config/steamAuth.js
@@ -1,6 +1,11 @@
 const SteamAuth = require("node-steam-openid");
 require('dotenv').config({path: './config/.env'})
 
+if (!process.env.STEAM_API_KEY) {
+    console.log("-------------------------------------------------------------");
+    console.error("🐠 STEAM_API_KEY is not set in .env file! If you don't have one, grab one from https://steamcommunity.com/dev/apikey 🐠")
+    process.exit(1)
+}
 
 // NEED TO ADD HTTPS IN PROD
 const steam = new SteamAuth({
