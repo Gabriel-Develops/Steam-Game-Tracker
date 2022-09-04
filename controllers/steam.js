582467a9d86bfc1c40a1d977673570ec90da6d1f
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-03 21:28:23 -0500

    Fetch and render game data from steam.js (...) - Add extended game data request from steam web api to fetch parameters - Pre-sort game data received by total hours played. - Replace array of game app IDs with game objects, stored as an embedded game object within the User model. - Pass this array of game objects as games to dashboard for rendering.

--- controllers/steam.js
+++ controllers/steam.js
@@ -36,37 +36,37 @@ module.exports = {
   //req.params = { steamID: < user steam ID here> }
   //also having ensureAuth passes in the current user
   getGames: async (req, res) => {
-    console.log(req.user)
-      try {
-        // fetch req in here, can't normally fetch from the server so installed node-fetch pkg and required it in server.js and here in the steam controller
-        // requesting the user's owned games in json format
-        console.log("Requesting with Steam ID:", req.user.steamID)
-        const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.user.steamID}&format=json`);
-        const ownedGames = await response.json()
-        const games = []
-        // if there are games to add, pushing each appID to the games array
-        if(ownedGames.response.games > req.user.gamesOwned){
-          ownedGames.response.games.forEach(game => {
-           games.push(game.appid)
-        })
-        // updating the user DB to reflect any updates
-        await User.updateOne({"_id": req.user.id}, {$set: {
-          gamesOwned: games,
-        }})
-        console.log('Successfully updated user')
-        }
-        res.render('dashboard.ejs', {user: req.user})
-        } catch (err){
-          console.log(err)
-          res.redirect('/')
-        }
+    console.log("req.user from steamController.getGames()", req.user)
+    try {
+      // fetch req in here, can't normally fetch from the server so installed node-fetch pkg and required it in server.js and here in the steam controller
+      // requesting the user's owned games in json format
+      // Retrieve the user's owned games using the WebAPI Key provided in .env, along with the user's steam ID, and include extended info for each game.
+      // TODO: Automate setting up the WebAPI Key per user and storing in User's document in db, so that a user doesn't need to make their profile public to make use of these features.
+      const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.user.steamID}&format=json&include_appinfo=true`)
+      const ownedGames = await response.json()
+      // Presort games by total time played in descending order
+      const ownedGamesSorted = ownedGames.response.games.sort((a, b) => {
+        return b.playtime_forever - a.playtime_forever
+      })
+
+      // updating the user DB to reflect any updates
+      await User.updateOne({"_id": req.user.id}, {$set: {
+        ownedGames: ownedGamesSorted,
+      }})
+      console.log('Successfully updated user')
+      res.render('dashboard.ejs', {user: req.user, games: ownedGamesSorted})
+    }
+    catch (err) {
+      console.log(err)
+      res.redirect('/')
+    }
   },
 
   //req.params = { steamID: < user steam ID here>, appId: < steam appID here > }
-  // getGameData: (req,res) => {
-  //   console.log(req.params)
-  //   res.render('todos.ejs')
-  // }
+  getGameData: (req,res) => {
+    console.log(req.params)
+    res.render('dashboard.ejs')
+  }
 }
 
 // sends user to a steam page to login
