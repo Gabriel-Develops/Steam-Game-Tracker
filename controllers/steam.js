b462f12c1479091bc1aea19fe98513d615ff775d
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-05 00:36:03 -0500

    Refactor function expressions to ES6 declarations within steam object exports (...) - Add comments for more info - Fix formatting for consistent spacing/indenting

--- controllers/steam.js
+++ controllers/steam.js
@@ -4,67 +4,68 @@ const steam = require('../middleware/steam')
 require('dotenv').config({path: './config/.env'})
 
 module.exports = {
-  steamLogin: async (req, res) => {
-    console.log(req.params)
-    try {
-      const redirectUrl = await steamAuth.getRedirectUrl();
-      return res.redirect(redirectUrl);
-    } catch (error) {
-      console.log(error)
-    }
-  },
+    // Function declarations are possible within object literals as of ES6. No need to define as key/value pairs.
+    // For more info: https://mariuszprzydatek.com/2017/04/22/ecmascript-es6-es2015-changes-overview
+    // Section 12: "function expressions in object literals vs. method definitions"
+    async steamLogin(req, res) {
+        console.log(req.params)
+        try {
+            const redirectUrl = await steamAuth.getRedirectUrl();
+            return res.redirect(redirectUrl);
+        } catch (error) {
+            console.log(error)
+        }
+    },
 
-  updateUser: async (req, res) => {
-    console.log(req.params)
-    try {
-      const user = await steamAuth.authenticate(req);
-      console.log(user)
-      // Updates user in db with steamId and steamUsername
-      await User.updateOne({"_id": req.user.id}, {$set: {
-        steamUserName: `${user.username}`,
-        steamID: `${user.steamid}`
-      }})
-      // updates the user with steamID, owned games, etc
-      console.log('Successfully updated user')
-      //redirect to dashboard
-      res.redirect(`/steam/${req.user.steamID}`)
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  
-  //req.params = { steamID: < user steam ID here> }
-  //also having ensureAuth passes in the current user
-  getGames: async (req, res) => {
-    try {
-      let ownedGamesSorted = await steam.getSortedGames(req)
-//      ownedGamesSorted.map((game) => {
-//                game.total_achievements= getGameAchievements(game.appid)
-//                game.user_achievements = getUserGameAchievements(game.appid,req.params.steamID)
-//                return game
-//            })
-      const playerIsPublic = await steam.getPlayerPublicStatus(req)
-      console.log("🐟 Player is public?", playerIsPublic)
-      // updating the user DB to reflect any updates
-      await User.updateOne({"_id": req.user.id}, {$set: {
-        ownedGames: ownedGamesSorted,
-      }})
+    async updateUser(req, res) {
+        console.log(req.params)
+        try {
+            const user = await steamAuth.authenticate(req);
+            console.log(user)
+            // Updates user in db with steamId and steamUsername
+            await User.updateOne({"_id": req.user.id}, {
+                $set: {
+                    steamUserName: `${user.username}`,
+                    steamID: `${user.steamid}`
+                }
+            })
+            // updates the user with steamID, owned games, etc
+            console.log('Successfully updated user')
+            //redirect to dashboard
+            res.redirect(`/steam/${req.user.steamID}`)
+        } catch (error) {
+            console.log(error);
+        }
+    },
 
-      res.render('dashboard.ejs', {
-        user: req.user,
-        games: ownedGamesSorted,
-        playerIsPublic
-      })
-    }
-    catch (err) {
-      console.log(err)
-      res.redirect('/')
-    }
-  },
+    //req.params = { steamID: < user steam ID here> }
+    //also having ensureAuth passes in the current user
+    async getGames(req, res) {
+        try {
+            let ownedGamesSorted = await steam.getSortedGames(req)
+            const playerIsPublic = await steam.getPlayerPublicStatus(req)
+            console.log("🐟 Player is public?", playerIsPublic)
+            // updating the user DB to reflect any updates
+            await User.updateOne({"_id": req.user.id}, {
+                $set: {
+                    ownedGames: ownedGamesSorted,
+                }
+            })
 
-  //req.params = { steamID: < user steam ID here>, appId: < steam appID here > }
-  getGameData: (req,res) => {
-    console.log(req.params)
-    res.render('dashboard.ejs')
-  }
+            res.render('dashboard.ejs', {
+                user: req.user,
+                games: ownedGamesSorted,
+                playerIsPublic
+            })
+        } catch (err) {
+            console.log(err)
+            res.redirect('/')
+        }
+    },
+    // Currently not in use
+    //req.params = { steamID: < user steam ID here>, appId: < steam appID here > }
+    getGameData(req, res) {
+        console.log(req.params)
+        res.render('dashboard.ejs')
+    }
 }
