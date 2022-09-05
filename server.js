5b6c171e5da205cc873270d20f13c56f8dd5ad23
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-05 00:38:31 -0500

    Refactor server.js (...) - Remove duplicated middleware expressions. - Move new middleware expressions to middleware setup function for organization (helps to ensure future order of operations as code gets more complicated). - Remove redundant imports (not used in this file) - Fix code formatting for consistency with indentation

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
+// Imports
 const express = require('express')
 const app = express()
-const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const passport = require('passport')
-const steamAuth = require('node-steam-openid')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
+// MongoDB connection
 const connectDB = require('./config/database')
+// Route imports
 const mainRoutes = require('./routes/main')
-
-
-// Setup config
-require('dotenv').config({path: './config/.env'})
-require('./config/passport')(passport)
-require('./config/steamAuth')
-
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/main')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use('*/js',express.static('public/js'));
-app.use('*/css',express.static('public/css'));
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
-
 const todoRoutes = require('./routes/todos')
 const steamRoutes = require('./routes/steam')
-const fetch = require('node-fetch')
+
 
 // Setup config
 require('dotenv').config({path: './config/.env'})
@@ -41,17 +24,21 @@ require('./config/steamAuth')
 
 function setupMiddleware() {
     app.set('view engine', 'ejs')
+    app.set('views', __dirname + '/views')
+    app.set('layout', 'layouts/main')
+    app.use(expressLayouts)
     app.use(express.static('public'))
-    app.use(express.urlencoded({ extended: true }))
+    app.use('*/js', express.static('public/js'));
+    app.use('*/css', express.static('public/css'));
+    app.use(express.urlencoded({extended: true}))
     app.use(express.json())
     app.use(logger('dev'))
     // Sessions
-    app.use(
-        session({
-            secret: 'keyboard cat',
-            resave: false,
-            saveUninitialized: false,
-            store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongooseConnection: mongoose.connection})
         })
     )
     // Passport middleware
@@ -73,11 +60,12 @@ async function startServer() {
     await connectDB()
     setupMiddleware()
     setupRoutes()
-    app.listen(process.env.PORT, ()=>{
+    app.listen(process.env.PORT, () => {
         console.log('Server is running, you better catch it!')
     })
 }
 
 
 // ----------------------------------------
+// This is the entry point!
 startServer()
